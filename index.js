bfd2215c31e223676a85a7beaf2fb532d96446b4
indaydream666 <69410145+indaydream666@users.noreply.github.com>
2020-10-01 12:19:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const axios = require("axios")
-const urls = ["https://script4-discord.glitch.me"]
+const urls = ["k"]
 setInterval(function() {
             urls.forEach(url => {
             axios.get(url).then(console.log("Pong at " + Date.now())).catch(() => {});
